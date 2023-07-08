c102ac0759a4c4abe840e53edf5e106d429b9c37
nate <nathanschnitzer587@gmail.com>
2023-07-08 16:24:53 -0400

    Formalized print registers

--- test_gdb_ext.py
+++ test_gdb_ext.py
@@ -22,4 +22,25 @@ class TestCommand(GenericCommand):
             print(f"get.{reg} = {reg_data}")
         # regs = Architecture.registers
 
-register_external_command(TestCommand())
+# register_external_command(TestCommand())
+
+
+class Print_X86_64_Registers(GenericCommand):
+    """ Command to Print Values of x86 Registers To stdout """
+    _cmdline_ = "print_registers_x86"
+    _syntax_  = f"{_cmdline_}"
+
+    @only_if_gdb_running
+    def do_invoke(self,argv):
+        frame = gdb.selected_frame()
+
+        print("Register Values:")
+        registers = X86_64.all_registers
+        for reg in registers:
+            # read_register does not like the '$' at the beginning of the register name
+            reg_data = frame.read_register(reg[1:])
+            print(f"{reg} -> {reg_data}")
+
+register_external_command(Print_X86_64_Registers())
+
+
